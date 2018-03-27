04cf101a4ae4a38e105a44160b004434950bbe83
kroxitrock <kroxitrock@gmail.com>
2018-03-27 16:06:43 +0300

    clean repo

--- src/main/Java/org/elsys/ip/books/config/ApplicationConfig.java
+++
@@ -1,6 +0,0 @@
-package org.elsys.ip.books.config;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class ApplicationConfig extends Application {}
